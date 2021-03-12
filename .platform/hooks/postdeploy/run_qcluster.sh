c5ea6843b8fead560f5aa19ac8da70eb343e5b2d
luisferhenriquez <luisferhenriquez@gmail.com>
2021-03-11 20:56:21 -0500

    eb config

---
+++ .platform/hooks/postdeploy/run_qcluster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /var/app/venv/*/bin/activate
+cd /var/app/staging
+python probetspp/manage.py qcluster --settings=probetspp.settings.production
--- probetspp/probetspp/settings/production.py
+++ probetspp/probetspp/settings/production.py
@@ -1,4 +1,7 @@
 from .common import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -21,3 +24,17 @@ STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
 STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_URL'),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    environment=os.getenv('ENVIRONMENT', 'alpha')
+)
