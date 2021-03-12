c8ba3655e8376418ddeafe8b571eb1d3466a74bd
luisferhenriquez <luisferhenriquez@gmail.com>
2021-03-11 21:46:01 -0500

    fix eb config

--- .platform/hooks/postdeploy/run_qcluster.sh
+++ .platform/hooks/postdeploy/run_qcluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /var/app/venv/*/bin/activate
-cd /var/app/staging
-python probetspp/manage.py qcluster --settings=probetspp.probetspp.settings.production --pythonpath /var/app/staging/
+cd /var/app/current
+python probetspp/manage.py qcluster --settings=probetspp.probetspp.settings.production --pythonpath /var/app/current/
