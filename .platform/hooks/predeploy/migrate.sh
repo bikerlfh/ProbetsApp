06a135f882663fbb6ae253a9cd896fbaad0427c8
luisferhenriquez <luisferhenriquez@gmail.com>
2021-03-12 21:04:44 -0500

    config

--- .platform/hooks/predeploy/migrate.sh
+++ .platform/hooks/predeploy/migrate.sh
@@ -4,5 +4,5 @@ source /var/app/venv/*/bin/activate
 cd /var/app/staging
 if [[ "$EB_IS_COMMAND_LEADER" == "true" ]]; then
     # leader
-    python probetspp/manage.py migrate --settings=probetspp.probetspp.settings.custom_manager --pythonpath /var/app/current/ --noinput
+    python probetspp/manage.py migrate --settings=probetspp.probetspp.settings.production --pythonpath /var/app/current/ --noinput
 fi
