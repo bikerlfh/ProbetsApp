695823f1558c59e451b224258e70a5fa2995ef42
luisferhenriquez <luisferhenriquez@gmail.com>
2021-03-14 12:31:15 -0500

    fix flash names

--- probetspp/apps/third_parties/flashscore/constants.py
+++ probetspp/apps/third_parties/flashscore/constants.py
@@ -8,7 +8,7 @@ FILES_PATH = getenv('FILES_PATH', pathlib.Path().absolute())
 TABLE_TENNIS_TODAY_URL = 'https://www.flashscore.co/tenis-de-mesa/'
 FOLDER_PATH_FLASH_DATA = f'{FILES_PATH}/info_pages/flash/'
 FILE_PATH_DATA_SET = f'{FILES_PATH}/datasets/%Y%m%d.csv'
-FILENAME_FORMAT_FLASH_DATA = '%d%m%Y.html'
+FILENAME_FORMAT_FLASH_DATA = '%Y%m%d.html'
 
 
 class TableTennisStatus(Enum):
