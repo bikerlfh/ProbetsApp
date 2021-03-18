ed25b971482bae9e7bd7f0b28b7686b67e965de8
luisferhenriquez <luisferhenriquez@gmail.com>
2021-03-17 22:13:43 -0500

    settings

--- probetspp/probetspp/settings/development.py
+++ probetspp/probetspp/settings/development.py
@@ -1,4 +1,6 @@
 from .common import *
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 DATABASES = {
     'default': {
@@ -10,7 +12,7 @@ DATABASES = {
         'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
-DEBUG = False
+DEBUG = True
 # For demo purposes only. Use a white list in the real world.
 CORS_ORIGIN_ALLOW_ALL = True
 SHELL_PLUS = "ipython"
@@ -22,3 +24,17 @@ INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS  # noqa
 # STATICFILES_DIRS = [
 #     '/tmp/probetspp/',
 # ]
+
+sentry_sdk.init(
+    dsn=os.getenv('SENTRY_URL'),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    environment=os.getenv('ENVIRONMENT', 'alpha')
+)
+
